481545738beaaebf54276ca706f3613d7ede0b16
chuks dozie <chuksdozie48@gmail.com>
2025-05-27 23:57:30 +0100

    Feat: Coursedata User Friendly

--- components/hero/HeroSection.tsx
+++ components/hero/HeroSection.tsx
@@ -29,15 +29,20 @@ const HeroSection = ({
       </video> */}
       {/* Overlay */}
       <div className="absolute inset-0 bg-black bg-opacity-60  h-[800px] max-lg:h-screen  z-10"></div>
-      <p className="text-sm font-light text-center w-[35%] text-gray-100 max-lg:w-[60%] max-md:w-[80%] z-10 max-lg:text-xs">
+      <p className="text-6xl font-bold text-center w-[60%] mt-6 text-gray-100 max-lg:w-[90%] max-lg:text-2xl z-10 mb-10">
+        {header || `Curated Learning for Your Tech Journey`}
+      </p>
+      <p className="text-sm font-light text-center w-[65%] text-gray-100 max-lg:w-[60%] max-md:w-[80%] z-10 max-lg:text-xs">
         {subHeader ||
           ` Explore a world of hand-picked courses designed to accelerate your
         skills in software development, UI/UX, and more. Learn at your pace,
         from beginner to expert, all in one place.`}
       </p>
-      <p className="text-6xl font-bold text-center w-[50%] mt-6 text-gray-100 max-lg:w-[90%] max-lg:text-2xl z-10 mb-10">
-        {header || `Curated Learning for Your Tech Journey`}
-      </p>
+      <div className=" flex w-full justify-center mt-2 md:hidden z-10">
+        <Link href={links.join_community} target="_blank">
+          <AppCtaBtn>Join Community</AppCtaBtn>
+        </Link>
+      </div>
 
       {/* <Link href={links.join_community} target="_blank" className="z-10">
         <AppCtaBtn>Join the Community</AppCtaBtn>
